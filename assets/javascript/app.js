7f125c38f79eaca5f1421efbde158709ea20c598
Eric J <ejohnston092@gmail.com>
2018-05-25 00:17:31 -0400

    Re-organized functions & added 5th question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,52 +5,15 @@ $(document).ready(function(){
     $("#done").hide();
     $("#submit").hide();
 
-    
     // Global Variables
     var timer = 45;
+    var seconds = 45;
     var timerID;
     var correctSoFar = 0;
     var incorrectSoFar = 0;
 
-    // Game Timer Function
-    function gameTimer(){
-        var seconds = 45;
-        clearInterval(timerID)
-        timerID = setInterval(function(){
-            seconds--;
-            $("#time-left").text(seconds + " seconds remaining!")
-            $("#results").empty();
-            if(seconds == 0){
-                $(".questions").hide();
-                $("#time-left").hide();
-                $("#submit").hide();
-                $("#done").show();
-                $("#start").show();
-                clearInterval(timerID);
-                clearInterval(seconds);
-                $("#results").html("<p>Correct answers: " + correctSoFar + "<p>Incorrect answers: " + incorrectSoFar)
-            }
-            else if($("#submit").click(function(){
-                clearInterval(timerID);
-                clearInterval(seconds);
-                $(".questions").hide();
-                $("#time-left").hide();
-                $("#submit").hide();
-                $("#done").show();
-                $("#start").show();
-                $("#results").html("<p>Correct answers: " + correctSoFar + "<p>Incorrect answers: " + incorrectSoFar)
-            }));
-        }, 1000);
-    };
-
-    // Function that updates the DOM with the time remaining
-    function showTime(){
-        $("#time-left").html(timer + " seconds remaining!")
-    }
-
-    // Begins trivia
-    $("#start").click(function(event){
-        event.preventDefault();
+    // This function initiates the game
+    function beginTrivia(){
         $("#results").empty();
         $("input[type='radio']").prop('checked', false);
         $("#start").hide();
@@ -62,6 +25,48 @@ $(document).ready(function(){
         gameTimer();
         correctSoFar = 0;
         incorrectSoFar = 0;
+    };
+
+    // Function that updates the DOM with the time remaining
+    function showTime(){
+        $("#time-left").html(timer + " seconds remaining!")
+    }
+    
+    // This function ends the game
+    function gameOver(){
+        clearInterval(timerID);
+        clearInterval(seconds);
+        $(".questions").hide();
+        $("#time-left").hide();
+        $("#submit").hide();
+        $("#done").show();
+        $("#start").show();
+        $("#results").html("<p>Correct answers: " + correctSoFar + "<p>Incorrect answers: " + incorrectSoFar)
+    }
+
+    // Game Timer Function
+    function gameTimer(){
+        var seconds = 45;
+        clearInterval(timerID)
+        timerID = setInterval(function(){
+            seconds--;
+            $("#time-left").text(seconds + " seconds remaining!")
+            $("#results").empty();
+            // If the timer reaches 0 seconds, the gameOver function initiates
+            if(seconds == 0){
+                gameOver();
+            }
+            // If the user clicks the submit button, the gameOver function initiates
+            else if($("#submit").click(function(){
+                gameOver();
+            }));
+        }, 1000);
+    };
+
+    // Begins trivia
+    $("#start").click(function(event){
+        event.preventDefault();
+        beginTrivia();
         });
 
     // Records correct answers
@@ -73,4 +78,4 @@ $(document).ready(function(){
     $("input[type='radio'][value='false']").click(function(){
         incorrectSoFar++;
     });
-}) 
+})
